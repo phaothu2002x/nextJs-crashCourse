edfb7d9c5755eb651dfc648b3d785b67b6f8b125
phaothu2002x <truongthuan21342@gmail.com>
2025-06-06 14:21:43 +0700

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-    return <h1 className="text-2xl heading">HomePage</h1>;
+    return (
+        <section className="pink_container">
+            <h1 className="text-2xl heading">
+                Pitch your startup <br />
+                Connect with entreprenuers
+            </h1>
+            <p className="sub-heading !max-w-3xl">
+                Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+                Competitions
+            </p>
+        </section>
+    );
 }
