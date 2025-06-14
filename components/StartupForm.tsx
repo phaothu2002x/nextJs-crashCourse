96951253a1b48c0350eeca60496c63d33348de20
phaothu2002x <truongthuan21342@gmail.com>
2025-06-14 16:07:27 +0700

    form submitting layout

---
+++ components/StartupForm.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import { useState } from 'react';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+import MDEditor from '@uiw/react-md-editor';
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
+const StartupForm = () => {
+    const [errors, setErrors] = useState<Record<string, string>>({});
+    const [pitch, setPitch] = useState('');
+    const isPending = false;
+    return (
+        <form action={() => {}} className="startup-form">
+            <div>
+                <label htmlFor="title" className="startup-form_label">
+                    Title
+                </label>
+                <Input
+                    id="title"
+                    name="title"
+                    placeholder="Startup Title"
+                    className="startup-form_input"
+                    required
+                />
+                {errors.title && (
+                    <p className="startup-form_error"> {errors.title}</p>
+                )}
+            </div>
+            <div>
+                <label htmlFor="description" className="startup-form_label">
+                    description
+                </label>
+                <Textarea
+                    id="description"
+                    name="description"
+                    placeholder="Startup description"
+                    className="startup-form_textarea"
+                    required
+                />
+                {errors.description && (
+                    <p className="startup-form_error"> {errors.description}</p>
+                )}
+            </div>
+            <div>
+                <label htmlFor="category" className="startup-form_label">
+                    Title
+                </label>
+                <Input
+                    id="category"
+                    name="category"
+                    placeholder="Startup Category (Tech, health, education, lifestyle, etc.)"
+                    className="startup-form_input"
+                    required
+                />
+                {errors.category && (
+                    <p className="startup-form_error"> {errors.category}</p>
+                )}
+            </div>
+            <div>
+                <label htmlFor="link" className="startup-form_label">
+                    Image Url
+                </label>
+                <Input
+                    id="link"
+                    name="link"
+                    placeholder="Startup Image Url"
+                    className="startup-form_input"
+                    required
+                />
+                {errors.link && (
+                    <p className="startup-form_error"> {errors.link}</p>
+                )}
+            </div>
+            <div data-color-mode="light">
+                <label htmlFor="pitch" className="startup-form_label">
+                    Pitch
+                </label>
+                <MDEditor
+                    id="pitch "
+                    height={300}
+                    value={pitch}
+                    preview="edit"
+                    style={{ borderRadius: 20, overflow: 'hidden' }}
+                    textareaProps={{
+                        placeholder:
+                            'Briefly describe your startup and what the problems it solves.',
+                    }}
+                    previewOptions={{ disallowedElements: ['style'] }}
+                    onChange={(value) => setPitch(value as string)}
+                />
+                {errors.pitch && (
+                    <p className="startup-form_error"> {errors.pitch}</p>
+                )}
+            </div>
+            <Button
+                type="submit"
+                className="startup-form_btn text-white"
+                disabled={isPending}
+            >
+                {isPending ? 'Submitting...' : 'Submit your Idea'}
+                <Send className="size-6 ml-2" />
+            </Button>
+        </form>
+    );
+};
+
+export default StartupForm;
